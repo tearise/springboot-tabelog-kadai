d41f7e9f8d6b0f6f3b7a4aeb5c064170f40c88fe
tearise <youtaiyanli@gmail.com>
2024-02-17 21:50:26 +0900

    github maik

---
+++ kadai_002/ngoyamesi/nagoyamasi/src/main/java/com/example/nagoyamesi/NagoyamasiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nagoyamesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagoyamasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagoyamasiApplication.class, args);
+	}
+
+}
