38c59f835405d51d15b22840558c31cc8f652d47
tearise <youtaiyanli@gmail.com>
2024-02-27 22:59:46 +0900

    deta

--- kadai_002/ngoyamesi/nagoyamasi/src/main/java/com/example/nagoyamesi/entity/CompanyIdentification.java
+++ kadai_002/ngoyamesi/nagoyamasi/src/main/java/com/example/nagoyamesi/entity/CompanyIdentification.java
@@ -3,11 +3,12 @@ package com.example.nagoyamesi.entity;
 import java.sql.Timestamp;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+@Entity
 @Table(name = "company_identification")
 public class CompanyIdentification {
     @Id
--- kadai_002/ngoyamesi/nagoyamasi/src/main/java/com/example/nagoyamesi/entity/User.java
+++ kadai_002/ngoyamesi/nagoyamasi/src/main/java/com/example/nagoyamesi/entity/User.java
@@ -3,12 +3,13 @@ package com.example.nagoyamesi.entity;
 import java.sql.Timestamp;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
+@Entity
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
