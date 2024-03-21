914d9fde2813d1c6879cf0549c2395006b16e4f4
tearise <youtaiyanli@gmail.com>
2024-03-21 21:46:09 +0900

    detail

--- kadai_002/ngoyamesi/nagoyamasi/src/main/java/com/example/nagoyamesi/controller/RestaurantController.java
+++ kadai_002/ngoyamesi/nagoyamasi/src/main/java/com/example/nagoyamesi/controller/RestaurantController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -51,4 +52,13 @@ public class RestaurantController {
         
         return "restaurants/index";
     }
+    
+    @GetMapping("/{id}")
+    public String show(@PathVariable(name = "id") Integer id, Model model) {
+    	Restaurant restaurant = restaurantRepository.getReferenceById(id);
+        
+        model.addAttribute("restaurant", restaurant);         
+        
+        return "restaurants/show";
+    }   
 }
